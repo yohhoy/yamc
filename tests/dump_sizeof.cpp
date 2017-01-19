e3cfc39f8591158e1ff4eb52127b70af746532d8
yoh <kawasaki.liamg@gmail.com>
2017-01-19 23:57:05 +0900

    add tests/dump_sizeof.cpp

---
+++ tests/dump_sizeof.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <condition_variable>
+#include <mutex>
+#include "naive_spin_mutex.hpp"
+#include "ttas_spin_mutex.hpp"
+#include "checked_mutex.hpp"
+#include "fair_mutex.hpp"
+
+
+#define DUMP(T) std::printf("%s %zu\n", #T, sizeof(T))
+
+
+int main()
+{
+  DUMP(int);
+  DUMP(std::size_t);
+  DUMP(std::thread::id);
+  DUMP(std::condition_variable);
+
+  DUMP(std::mutex);
+  DUMP(std::timed_mutex);
+  DUMP(std::recursive_mutex);
+  DUMP(std::recursive_timed_mutex);
+
+  DUMP(yamc::spin::mutex);
+  DUMP(yamc::spin_weak::mutex);
+  DUMP(yamc::spin_ttas::mutex);
+  DUMP(yamc::checked::mutex);
+  DUMP(yamc::checked::recursive_mutex);
+  DUMP(yamc::fair::mutex);
+  DUMP(yamc::fair::recursive_mutex);
+  return 0;
+}
