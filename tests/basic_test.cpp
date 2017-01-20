24dd2ec561a6ee88a2ea8c159a2d60ecd2e7eeb9
yoh <kawasaki.liamg@gmail.com>
2017-01-21 00:53:33 +0900

    update tests/basic_test.cpp
    - reduce loop iteration on BasicLock test
    - add TryLock, TryLockFail test

--- tests/basic_test.cpp
+++ tests/basic_test.cpp
@@ -12,7 +12,7 @@
 
 
 #define TEST_THREADS   8
-#define TEST_ITERATION 100000u
+#define TEST_ITERATION 10000u
 
 
 using NormalMutexTypes = ::testing::Types<
@@ -20,10 +20,7 @@ using NormalMutexTypes = ::testing::Types<
   yamc::spin_weak::mutex,
   yamc::spin_ttas::mutex,
   yamc::checked::mutex,
-  yamc::checked::recursive_mutex,
-  yamc::fair::mutex,
-  yamc::fair::recursive_mutex,
-  yamc::alternate::recursive_mutex
+  yamc::fair::mutex
 >;
 
 template <typename Mutex>
@@ -31,8 +28,7 @@ struct NormalMutexTest : ::testing::Test {};
 
 TYPED_TEST_CASE(NormalMutexTest, NormalMutexTypes);
 
-
-// normal mutex usecase
+// mutex::lock()
 TYPED_TEST(NormalMutexTest, BasicLock)
 {
   TypeParam mtx;
@@ -48,6 +44,44 @@ TYPED_TEST(NormalMutexTest, BasicLock)
   ASSERT_EQ(TEST_ITERATION * TEST_THREADS, counter);
 }
 
+// mutex::try_lock()
+TYPED_TEST(NormalMutexTest, TryLock)
+{
+  TypeParam mtx;
+  std::size_t counter = 0;
+  yamc::test::task_runner(
+    TEST_THREADS,
+    [&](std::size_t /*id*/) {
+      for (std::size_t n = 0; n < TEST_ITERATION; ++n) {
+        while (!mtx.try_lock()) {
+          std::this_thread::yield();
+        }
+        std::lock_guard<decltype(mtx)> lk(mtx, std::adopt_lock);
+        counter = counter + 1;
+      }
+    });
+  ASSERT_EQ(TEST_ITERATION * TEST_THREADS, counter);
+}
+
+// mutex::try_lock() failure
+TYPED_TEST(NormalMutexTest, TryLockFail)
+{
+  yamc::test::barrier step(2);
+  TypeParam mtx;
+  yamc::test::join_thread thd([&]{
+    ASSERT_NO_THROW(mtx.lock());
+    step.await();  // b1
+    step.await();  // b2
+    ASSERT_NO_THROW(mtx.unlock());
+  });
+  {
+    step.await();  // b1
+    ASSERT_EQ(false, mtx.try_lock());
+    step.await();  // b2
+  }
+}
+
+
 using RecursiveMutexTypes = ::testing::Types<
   yamc::checked::recursive_mutex,
   yamc::fair::recursive_mutex,
@@ -59,8 +93,7 @@ struct RecursiveMutexTest : ::testing::Test {};
 
 TYPED_TEST_CASE(RecursiveMutexTest, RecursiveMutexTypes);
 
-
-// recursive mutex usecase
+// recursive_mutex::lock()
 TYPED_TEST(RecursiveMutexTest, BasicLock)
 {
   TypeParam mtx;
@@ -70,9 +103,37 @@ TYPED_TEST(RecursiveMutexTest, BasicLock)
     [&](std::size_t /*id*/) {
       for (std::size_t n = 0; n < TEST_ITERATION; ++n) {
         std::lock_guard<decltype(mtx)> lk1(mtx);
-        ++c1;
+        auto before_cnt = ++c1;
         {
           std::lock_guard<decltype(mtx)> lk2(mtx);
+          ++c2;
+        }
+        auto after_cnt = ++c3;
+        ASSERT_TRUE(before_cnt == after_cnt);
+      }
+    });
+  ASSERT_EQ(TEST_ITERATION * TEST_THREADS, c1);
+  ASSERT_EQ(TEST_ITERATION * TEST_THREADS, c2);
+  ASSERT_EQ(TEST_ITERATION * TEST_THREADS, c3);
+}
+
+// recursive_mutex::try_lock()
+TYPED_TEST(RecursiveMutexTest, TryLock)
+{
+  TypeParam mtx;
+  std::size_t c1 = 0, c2 = 0, c3 = 0;
+  yamc::test::task_runner(
+    TEST_THREADS,
+    [&](std::size_t /*id*/) {
+      for (std::size_t n = 0; n < TEST_ITERATION; ++n) {
+        while (!mtx.try_lock()) {
+          std::this_thread::yield();
+        }
+        std::lock_guard<decltype(mtx)> lk1(mtx, std::adopt_lock);
+        ++c1;
+        {
+          ASSERT_EQ(true, mtx.try_lock());
+          std::lock_guard<decltype(mtx)> lk2(mtx, std::adopt_lock);
           ++c2;
         }
         ++c3;
@@ -83,3 +144,32 @@ TYPED_TEST(RecursiveMutexTest, BasicLock)
   ASSERT_EQ(TEST_ITERATION * TEST_THREADS, c3);
 }
 
+// recursive_mutex::try_lock() failure
+TYPED_TEST(RecursiveMutexTest, TryLockFail)
+{
+  yamc::test::barrier step(2);
+  TypeParam mtx;
+  yamc::test::join_thread thd([&]{
+    ASSERT_NO_THROW(mtx.lock());
+    step.await();  // b1
+    step.await();  // b2
+    ASSERT_NO_THROW(mtx.lock());
+    step.await();  // b3
+    step.await();  // b4
+    ASSERT_NO_THROW(mtx.unlock());
+    step.await();  // b5
+    step.await();  // b6
+    ASSERT_NO_THROW(mtx.unlock());
+  });
+  {
+    step.await();  // b1
+    ASSERT_EQ(false, mtx.try_lock());  // lockcnt = 1
+    step.await();  // b2
+    step.await();  // b3
+    ASSERT_EQ(false, mtx.try_lock());  // lockcnt = 2
+    step.await();  // b4
+    step.await();  // b5
+    ASSERT_EQ(false, mtx.try_lock());  // lockcnt = 3
+    step.await();  // b6
+  }
+}
