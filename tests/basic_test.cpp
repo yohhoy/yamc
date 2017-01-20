77edfeee2782345a7c39efef59279b0b08f3006a
yoh <kawasaki.liamg@gmail.com>
2017-01-20 18:20:36 +0900

    add RecursiveMutexTest.BasicLock test (basic_test.cpp)

--- tests/basic_test.cpp
+++ tests/basic_test.cpp
@@ -46,3 +46,37 @@ TYPED_TEST(NormalMutexTest, BasicLock)
   ASSERT_EQ(TEST_ITERATION * TEST_THREADS, counter);
 }
 
+using RecursiveMutexTypes = ::testing::Types<
+  yamc::checked::recursive_mutex,
+  yamc::fair::recursive_mutex
+>;
+
+template <typename Mutex>
+struct RecursiveMutexTest : ::testing::Test {};
+
+TYPED_TEST_CASE(RecursiveMutexTest, RecursiveMutexTypes);
+
+
+// recursive mutex usecase
+TYPED_TEST(RecursiveMutexTest, BasicLock)
+{
+  TypeParam mtx;
+  std::size_t c1 = 0, c2 = 0, c3 = 0;
+  yamc::test::task_runner(
+    TEST_THREADS,
+    [&](std::size_t /*id*/) {
+      for (std::size_t n = 0; n < TEST_ITERATION; ++n) {
+        std::lock_guard<decltype(mtx)> lk1(mtx);
+        ++c1;
+        {
+          std::lock_guard<decltype(mtx)> lk2(mtx);
+          ++c2;
+        }
+        ++c3;
+      }
+    });
+  ASSERT_EQ(TEST_ITERATION * TEST_THREADS, c1);
+  ASSERT_EQ(TEST_ITERATION * TEST_THREADS, c2);
+  ASSERT_EQ(TEST_ITERATION * TEST_THREADS, c3);
+}
+
