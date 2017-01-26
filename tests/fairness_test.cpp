e0766aaf120e1eebdeb125a764a2858c5ad21c92
yoh <kawasaki.liamg@gmail.com>
2017-01-26 14:03:44 +0900

    refine FairMutexTest test
    sleep on the critical path, and assert elapsed.

--- tests/fairness_test.cpp
+++ tests/fairness_test.cpp
@@ -10,8 +10,10 @@
 #include "yamc_testutil.hpp"
 
 
-#define WAIT_TICK \
-  std::this_thread::sleep_for(std::chrono::milliseconds(200))
+#define TEST_TICKS std::chrono::milliseconds(200)
+
+#define EXPECT_STEP(n_) \
+  { EXPECT_EQ(n_, ++step); std::this_thread::sleep_for(TEST_TICKS); }
 
 
 #if 0
@@ -42,51 +44,45 @@ TYPED_TEST(FairMutexTest, FifoSched)
   yamc::test::phaser phaser(3);
   int step = 0;
   TypeParam mtx;
-  yamc::test::join_thread thd0([&]{
-    auto ph = phaser.get(0);
-    WAIT_TICK;
-    TRACE("request... L1");
-    mtx.lock();                 // 1) acquire 1st lock
-    TRACE("acquire L1");
-    WAIT_TICK;
-    ph.advance(2);
-    WAIT_TICK;
-    ph.await();
-    WAIT_TICK;
-    EXPECT_EQ(1, ++step);
-    mtx.unlock();               // 4) unlock
-    TRACE("release L1");
+  yamc::test::stopwatch<> sw;
+  yamc::test::task_runner(3, [&](std::size_t id) {
+    auto ph = phaser.get(id);
+    switch (id) {
+    case 0:
+      TRACE("request... L1");
+      mtx.lock();                 // 1) acquire 1st lock
+      TRACE("acquire L1");
+      EXPECT_STEP(1)
+      ph.advance(2);
+      ph.await();
+      EXPECT_STEP(4)
+      mtx.unlock();               // 4) unlock
+      TRACE("release L1");
+      break;
+    case 1:
+      ph.await();
+      EXPECT_STEP(2)
+      ph.advance(2);
+      TRACE("request... L2");
+      mtx.lock();                 // 2) request 2nd lock
+      TRACE("acquire L2");
+      EXPECT_STEP(5)              // 5) acquire 2nd lock (before 3rd lock)
+      mtx.unlock();               // 6) unlock
+      TRACE("release L2");
+      break;
+    case 2:
+      ph.await();
+      ph.await();
+      EXPECT_STEP(3)
+      ph.advance(1);
+      TRACE("request... L3");
+      mtx.lock();                 // 3) request 3rd lock
+      TRACE("acquire L3");
+      EXPECT_STEP(6)              // 7) acquire 3rd lock (after 2nd unlock)
+      mtx.unlock();
+      TRACE("release L3");
+      break;
+    }
   });
-  yamc::test::join_thread thd1([&]{
-    auto ph = phaser.get(1);
-    WAIT_TICK;
-    ph.await();
-    WAIT_TICK;
-    ph.advance(2);
-    WAIT_TICK;
-    TRACE("request... L2");
-    mtx.lock();                 // 2) request 2nd lock
-    TRACE("acquire L2");
-    EXPECT_EQ(2, ++step);       // 5) acquire 2nd lock (before 3rd lock)
-    WAIT_TICK;
-    mtx.unlock();               // 6) unlock
-    TRACE("release L2");
-  });
-  {
-    auto ph = phaser.get(2);
-    WAIT_TICK;
-    ph.await();
-    WAIT_TICK;
-    ph.await();
-    WAIT_TICK;
-    ph.advance(1);
-    WAIT_TICK;
-    TRACE("request... L3");
-    mtx.lock();                 // 3) request 3rd lock
-    TRACE("acquire L3");
-    EXPECT_EQ(3, ++step);       // 7) acquire 3rd lock
-    WAIT_TICK;
-    mtx.unlock();
-    TRACE("release L3");
-  }
+  ASSERT_LE(TEST_TICKS * step, sw.elapsed());
 }
