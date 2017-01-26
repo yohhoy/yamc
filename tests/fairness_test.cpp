0dbcd792416dfa196134b92aece34f2f091c38a9
yoh <kawasaki.liamg@gmail.com>
2017-01-26 15:07:33 +0900

    add scheduling diagram in comment (fairness_test.cpp)

--- tests/fairness_test.cpp
+++ tests/fairness_test.cpp
@@ -39,6 +39,17 @@ struct FairMutexTest : ::testing::Test {};
 TYPED_TEST_CASE(FairMutexTest, FairMutexTypes);
 
 // FIFO scheduling
+//
+// T0: T=1=a=a=====w=4=U........
+//         |  \   /    |
+// T1: ....w.2.a.a.l---L=5=U....
+//         |   |  \        |
+// T2: ....w.t.w.3.a.l-----L=6=U
+//
+//   l/L=lock(request/acquired), U=unlock()
+//   T=try_lock(true), t=try_lock(false)
+//   a=phase advance, w=phase await
+//
 TYPED_TEST(FairMutexTest, FifoSched)
 {
   yamc::test::phaser phaser(3);
@@ -49,38 +60,30 @@ TYPED_TEST(FairMutexTest, FifoSched)
     auto ph = phaser.get(id);
     switch (id) {
     case 0:
-      TRACE("request... L1");
-      mtx.lock();                 // 1) acquire 1st lock
-      TRACE("acquire L1");
+      EXPECT_TRUE(mtx.try_lock());
       EXPECT_STEP(1)
-      ph.advance(2);
-      ph.await();
+      ph.advance(2);  // p1-2
+      ph.await();     // p3
       EXPECT_STEP(4)
-      mtx.unlock();               // 4) unlock
-      TRACE("release L1");
+      mtx.unlock();
       break;
     case 1:
-      ph.await();
+      ph.await();     // p1
       EXPECT_STEP(2)
-      ph.advance(2);
-      TRACE("request... L2");
-      mtx.lock();                 // 2) request 2nd lock
-      TRACE("acquire L2");
-      EXPECT_STEP(5)              // 5) acquire 2nd lock (before 3rd lock)
-      mtx.unlock();               // 6) unlock
-      TRACE("release L2");
+      ph.advance(2);  // p2-3
+      mtx.lock();
+      EXPECT_STEP(5)
+      mtx.unlock();
       break;
     case 2:
-      ph.await();
-      ph.await();
+      ph.await();     // p1
+      EXPECT_FALSE(mtx.try_lock());
+      ph.await();     // p2
       EXPECT_STEP(3)
-      ph.advance(1);
-      TRACE("request... L3");
-      mtx.lock();                 // 3) request 3rd lock
-      TRACE("acquire L3");
-      EXPECT_STEP(6)              // 7) acquire 3rd lock (after 2nd unlock)
+      ph.advance(1);  // p3
+      mtx.lock();
+      EXPECT_STEP(6)
       mtx.unlock();
-      TRACE("release L3");
       break;
     }
   });
