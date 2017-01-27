be82cd096b33fde8f35efab7632dc893a36ec172
yoh <kawasaki.liamg@gmail.com>
2017-01-27 20:41:11 +0900

    add SchedTryLock{For,Until} tests (fairness_test.cpp)

--- tests/fairness_test.cpp
+++ tests/fairness_test.cpp
@@ -10,12 +10,6 @@
 #include "yamc_testutil.hpp"
 
 
-#define TEST_TICKS std::chrono::milliseconds(200)
-
-#define EXPECT_STEP(n_) \
-  { EXPECT_EQ(n_, ++step); std::this_thread::sleep_for(TEST_TICKS); }
-
-
 #if 0
 namespace {
 std::mutex g_guard;
@@ -26,6 +20,14 @@ std::mutex g_guard;
 #else
 #define TRACE(msg_)
 #endif
+
+
+#define TEST_EXPECT_TIMEOUT std::chrono::milliseconds(10)
+#define TEST_NOT_TIMEOUT std::chrono::minutes(3)
+#define TEST_TICKS std::chrono::milliseconds(200)
+
+#define EXPECT_STEP(n_) \
+  { TRACE("STEP"#n_); EXPECT_EQ(n_, ++step); std::this_thread::sleep_for(TEST_TICKS); }
 
 
 using FairMutexTypes = ::testing::Types<
@@ -41,11 +43,11 @@ TYPED_TEST_CASE(FairMutexTest, FairMutexTypes);
 
 // FIFO scheduling
 //
-// T0: T=1=a=a=====w=4=U........
-//         |  \   /    |
-// T1: ....w.2.a.a.l---L=5=U....
-//         |   |  \        |
-// T2: ....w.t.w.3.a.l-----L=6=U
+// T0: T=1=a=a=====w=4=U.......L=7=U
+//         |  \   /    |       |
+// T1: ....w.2.a.a.l---L=5=U........
+//         |   |  \        |   |
+// T2: ....w.t.w.3.a.l-----L=6=U....
 //
 //   l/L=lock(request/acquired), U=unlock()
 //   T=try_lock(true), t=try_lock(false)
@@ -67,6 +69,9 @@ TYPED_TEST(FairMutexTest, FifoSched)
       ph.await();     // p3
       EXPECT_STEP(4)
       mtx.unlock();
+      mtx.lock();
+      EXPECT_STEP(7)
+      mtx.unlock();
       break;
     case 1:
       ph.await();     // p1
@@ -83,6 +88,123 @@ TYPED_TEST(FairMutexTest, FifoSched)
       EXPECT_STEP(3)
       ph.advance(1);  // p3
       mtx.lock();
+      EXPECT_STEP(6)
+      mtx.unlock();
+      break;
+    }
+  });
+  ASSERT_LE(TEST_TICKS * step, sw.elapsed());
+}
+
+
+using FairTimedMutexTypes = ::testing::Types<
+  yamc::fair::timed_mutex
+>;
+
+template <typename Mutex>
+struct FairTimedMutexTest : ::testing::Test {};
+
+TYPED_TEST_CASE(FairTimedMutexTest, FairTimedMutexTypes);
+
+// FIFO scheduling with try_lock_for()
+//
+// T0: T=1=a=====a=====w=4=U........
+//         |      \   /    |
+// T1: ....w.......w.a.t---T=5=U....
+//         |       |  \        |
+// T2: ....w.t-2-*.w.3.a.t-----T=6=U
+//
+//   t/T=try_lock_for(request/acquired)
+//   U=unlock(), *=timeout
+//   a=phase advance, w=phase await
+//
+TYPED_TEST(FairTimedMutexTest, FifoTryLockFor)
+{
+  yamc::test::phaser phaser(3);
+  int step = 0;
+  TypeParam mtx;
+  yamc::test::stopwatch<> sw;
+  yamc::test::task_runner(3, [&](std::size_t id) {
+    auto ph = phaser.get(id);
+    switch (id) {
+    case 0:
+      EXPECT_TRUE(mtx.try_lock_for(TEST_NOT_TIMEOUT));
+      EXPECT_STEP(1)
+      ph.advance(2);  // p1-2
+      ph.await();     // p3
+      EXPECT_STEP(4)
+      mtx.unlock();
+      break;
+    case 1:
+      ph.await();     // p1
+      ph.await();     // p2
+      ph.advance(1);  // p3
+      EXPECT_TRUE(mtx.try_lock_for(TEST_NOT_TIMEOUT));
+      EXPECT_STEP(5)
+      mtx.unlock();
+      break;
+    case 2:
+      ph.await();     // p1
+      EXPECT_EQ(2, ++step);
+      EXPECT_FALSE(mtx.try_lock_for(TEST_TICKS));
+      ph.await();     // p2
+      EXPECT_STEP(3)
+      ph.advance(1);  // p3
+      EXPECT_TRUE(mtx.try_lock_for(TEST_NOT_TIMEOUT));
+      EXPECT_STEP(6)
+      mtx.unlock();
+      break;
+    }
+  });
+  ASSERT_LE(TEST_TICKS * step, sw.elapsed());
+}
+
+// FIFO scheduling with try_lock_until()
+//
+// T0: T=1=a=====a=====w=4=U........
+//         |      \   /    |
+// T1: ....w.......w.a.t---L=5=U....
+//         |       |  \        |
+// T2: ....w.t-2-*.w.3.a.t-----L=6=U
+//
+//   t/T=try_lock_until(request/acquired)
+//   U=unlock(), *=timeout
+//   a=phase advance, w=phase await
+//
+TYPED_TEST(FairTimedMutexTest, FifoTryLockUntil)
+{
+  yamc::test::phaser phaser(3);
+  int step = 0;
+  TypeParam mtx;
+  using Clock = std::chrono::steady_clock;
+  yamc::test::stopwatch<> sw;
+  yamc::test::task_runner(3, [&](std::size_t id) {
+    auto ph = phaser.get(id);
+    switch (id) {
+    case 0:
+      EXPECT_TRUE(mtx.try_lock_until(Clock::now() + TEST_NOT_TIMEOUT));
+      EXPECT_STEP(1)
+      ph.advance(2);  // p1-2
+      ph.await();     // p3
+      EXPECT_STEP(4)
+      mtx.unlock();
+      break;
+    case 1:
+      ph.await();     // p1
+      ph.await();     // p2
+      ph.advance(1);  // p3
+      EXPECT_TRUE(mtx.try_lock_until(Clock::now() + TEST_NOT_TIMEOUT));
+      EXPECT_STEP(5)
+      mtx.unlock();
+      break;
+    case 2:
+      ph.await();     // p1
+      EXPECT_EQ(2, ++step);
+      EXPECT_FALSE(mtx.try_lock_until(Clock::now() + TEST_TICKS));
+      ph.await();     // p2
+      EXPECT_STEP(3)
+      ph.advance(1);  // p3
+      EXPECT_TRUE(mtx.try_lock_until(Clock::now() + TEST_NOT_TIMEOUT));
       EXPECT_STEP(6)
       mtx.unlock();
       break;
