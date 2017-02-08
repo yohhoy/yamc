4d8eb08cf182447ad0d14491040a413531fab461
yoh <kawasaki.liamg@gmail.com>
2017-02-08 18:32:21 +0900

    update perf_rwlock: measure std::mutex, fair::mutex as baseline

--- tests/perf_rwlock.cpp
+++ tests/perf_rwlock.cpp
@@ -6,9 +6,11 @@
 #include <iostream>
 #include <iterator>
 #include <numeric>
+#include <mutex>
 #include <thread>
 #include <vector>
 #include "alternate_shared_mutex.hpp"
+#include "fair_mutex.hpp"
 #include "fair_shared_mutex.hpp"
 #include "yamc_testutil.hpp"
 
@@ -114,16 +116,88 @@ void perform_rwlock_contention(const config& cfg)
 }
 
 
-template <typename SharedMutex>
-void perf_run(const char* title, unsigned nthread)
+template <typename Mutex>
+void perform_lock_contention(const config& cfg)
 {
+  std::size_t nthread = cfg.nwriter + cfg.nreader;
+  yamc::test::barrier gate(nthread + 1);
+  std::vector<std::thread> thds;
+  std::vector<std::size_t> counters(nthread);
 
+  std::atomic<int> running = {1};
+  Mutex mtx;
+
+  // writer/reader threads
+  for (std::size_t i = 0; i < nthread; i++) {
+    std::size_t idx = i;
+    thds.emplace_back([&,idx]{
+      std::size_t ncount = 0;
+      gate.await();  // start
+      while (running.load(std::memory_order_relaxed)) {
+        mtx.lock();
+        PERF_DUMMY_TASK(PERF_WEIGHT_TASK)
+        ++ncount;  // write/read op
+        mtx.unlock();
+        PERF_DUMMY_TASK(PERF_WEIGHT_WAIT)
+        std::this_thread::yield();
+      }
+      gate.await();  // end
+      counters[idx] = ncount;
+    });
+  }
+
+  // run measurement
+  yamc::test::stopwatch<> sw;
+  gate.await();  // start
+  std::this_thread::sleep_for(PERF_DURATION);
+  running.store(0, std::memory_order_relaxed);
+  gate.await();  // end
+  double elapsed = (double)sw.elapsed().count() / 1000000.;  // [sec]
+
+  // summarize write/read issue count
+  for (auto& t : thds) {
+    t.join();
+  }
+  std::size_t nissue = std::accumulate(counters.begin(), counters.end(), 0u);
+
+  // average [count/sec/thread]
+  double avg = (double)nissue / nthread / elapsed;
+  // SD(standard deviation) [count/sec/thread]
+  double sd = std::sqrt(std::accumulate(counters.begin(), counters.end(), 0.,
+                                         [avg, elapsed](double acc, std::size_t v) {
+                                            return acc + (v / elapsed - avg) * (v / elapsed - avg);
+                                         }) / nthread);
+
+  // print result
+  std::cout
+    << nthread << '\t' << nissue << '\t' << avg << '\t' << sd << "\t-\t-\t-\t-" << std::endl;
+}
+
+
+void print_header(const char* title, unsigned nthread)
+{
   std::cout
     << "# " << title
     << " ncpu=" << std::thread::hardware_concurrency() << " nthread=" << nthread
     << " task/wait=" << PERF_WEIGHT_TASK << "/" << PERF_WEIGHT_WAIT
     << " duration=" << PERF_DURATION.count() << std::endl;
+}
 
+
+template <typename Mutex>
+void perf_lock(const char* title, unsigned nthread)
+{
+  print_header(title, nthread);
+  std::cout << "# Wt/Rd\t[raw]\t[ops]\t[sd]\t-\t-\t-\t-" << std::endl;
+  perform_lock_contention<Mutex>({ nthread, 0 });
+  std::cout << "\n\n" << std::flush;
+}
+
+
+template <typename SharedMutex>
+void perf_rwlock(const char* title, unsigned nthread)
+{
+  print_header(title, nthread);
   std::cout << "# Write\t[raw]\t[ops]\t[sd]\tRead\t[raw]\t[ops]\t[sd]" << std::endl;
   for (unsigned nwt = 1; nwt < nthread; nwt++) {
     config cfg = { nwt, nthread - nwt };
@@ -136,10 +210,13 @@ void perf_run(const char* title, unsigned nthread)
 int main()
 {
   unsigned nthread = 10;
-
   using reader_prefer_shared_mutex = yamc::alternate::basic_shared_mutex<yamc::rwlock::ReaderPrefer>;
   using writer_prefer_shared_mutex = yamc::alternate::basic_shared_mutex<yamc::rwlock::WriterPrefer>;
-  perf_run<reader_prefer_shared_mutex>("ReaderPrefer", nthread);
-  perf_run<writer_prefer_shared_mutex>("WriterPrefer", nthread);
-  perf_run<yamc::fair::shared_mutex>("PhaseFair", nthread);
+
+  perf_lock<std::mutex>       ("StdMutex", nthread);
+  perf_lock<yamc::fair::mutex>("FifoMutex", nthread);
+
+  perf_rwlock<reader_prefer_shared_mutex>("ReaderPrefer", nthread);
+  perf_rwlock<writer_prefer_shared_mutex>("WriterPrefer", nthread);
+  perf_rwlock<yamc::fair::shared_mutex> ("PhaseFair", nthread);
 }
--- tests/perf_rwlock.sh
+++ tests/perf_rwlock.sh
@@ -12,17 +12,17 @@ set xlabel "writer threads"
 set ylabel "ops/sec/thread"
 set xrange [0.5:9.5]
 
-plot "${DATFILE}" index 0 using 1:3   with lines     lt 1 title "ReaderPerfer/WriteLock", \
-     "${DATFILE}" index 0 using 1:3:4 with errorbars lt 1 notitle, \
-     "${DATFILE}" index 0 using 1:7   with lines     lt 2 title "ReaderPerfer/ReadLock", \
-     "${DATFILE}" index 0 using 1:7:8 with errorbars lt 2 notitle, \
-     "${DATFILE}" index 1 using 1:3   with lines     lt 3 title "WriterPerfer/WriteLock", \
-     "${DATFILE}" index 1 using 1:3:4 with errorbars lt 3 notitle, \
-     "${DATFILE}" index 1 using 1:7   with lines     lt 4 title "WriterPerfer/ReadLock", \
-     "${DATFILE}" index 1 using 1:7:8 with errorbars lt 4 notitle, \
-     "${DATFILE}" index 2 using 1:3   with lines     lt 5 title "PhaseFair/WriteLock", \
-     "${DATFILE}" index 2 using 1:3:4 with errorbars lt 5 notitle, \
-     "${DATFILE}" index 2 using 1:7   with lines     lt 6 title "PhaseFair/ReadLock", \
-     "${DATFILE}" index 2 using 1:7:8 with errorbars lt 6 notitle,
+plot "${DATFILE}" index 2 using 1:3   with lines     lt 1 title "ReaderPerfer/WriteLock", \
+     "${DATFILE}" index 2 using 1:3:4 with errorbars lt 1 notitle, \
+     "${DATFILE}" index 2 using 1:7   with lines     lt 2 title "ReaderPerfer/ReadLock", \
+     "${DATFILE}" index 2 using 1:7:8 with errorbars lt 2 notitle, \
+     "${DATFILE}" index 3 using 1:3   with lines     lt 3 title "WriterPerfer/WriteLock", \
+     "${DATFILE}" index 3 using 1:3:4 with errorbars lt 3 notitle, \
+     "${DATFILE}" index 3 using 1:7   with lines     lt 4 title "WriterPerfer/ReadLock", \
+     "${DATFILE}" index 3 using 1:7:8 with errorbars lt 4 notitle, \
+     "${DATFILE}" index 4 using 1:3   with lines     lt 5 title "PhaseFair/WriteLock", \
+     "${DATFILE}" index 4 using 1:3:4 with errorbars lt 5 notitle, \
+     "${DATFILE}" index 4 using 1:7   with lines     lt 6 title "PhaseFair/ReadLock", \
+     "${DATFILE}" index 4 using 1:7:8 with errorbars lt 6 notitle,
 EOT
 echo "output: ${PNGFILE}"
