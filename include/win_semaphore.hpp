4aab1194f7f2964e347f4d737a6888a6a371f9de
yoh <kawasaki.liamg@gmail.com>
2019-08-05 12:40:25 +0900

    improve error check (win_semaphore.hpp)

--- include/win_semaphore.hpp
+++ include/win_semaphore.hpp
@@ -29,6 +29,7 @@
 #include <cassert>
 #include <cstddef>
 #include <chrono>
+#include <system_error>
 // Windows semaphore
 #include <windows.h>
 
@@ -40,7 +41,7 @@
  * - yamc::win::binary_semaphore
  *
  * This implementation use native Win32 semaphore.
- * https://docs.microsoft.com/en-us/windows/win32/sync/semaphore-objects
+ * https://docs.microsoft.com/windows/win32/sync/semaphore-objects
  */
 namespace yamc {
 
@@ -48,15 +49,32 @@ namespace win {
 
 template <std::ptrdiff_t least_max_value = 0x7FFFFFFF>
 class counting_semaphore {
-  ::HANDLE hsem_;
+  ::HANDLE hsem_ = NULL;
+
+  void validate_native_handle(const char* what_arg)
+  {
+    if (hsem_ == NULL) {
+      // [thread.mutex.requirements.mutex]
+      // invalid_argument - if any native handle type manipulated as part of mutex construction is incorrect.
+      throw std::system_error(std::make_error_code(std::errc::invalid_argument), what_arg);
+    }
+  }
 
   template<class Rep, class Period>
   bool do_try_acquirewait(const std::chrono::duration<Rep, Period>& timeout)
   {
-    DWORD msec = (DWORD)std::chrono::duration_cast<std::chrono::milliseconds>(timeout).count();
+    using namespace std::chrono;
     // WaitForSingleObject() timeout has millisecond precision.
-    // https://docs.microsoft.com/en-us/windows/win32/api/synchapi/nf-synchapi-waitforsingleobject
-    DWORD result = ::WaitForSingleObject(hsem_, msec);
+    // https://docs.microsoft.com/windows/win32/api/synchapi/nf-synchapi-waitforsingleobject
+    // https://docs.microsoft.com/windows/win32/sync/wait-functions
+    // timeout_in_msec = std::chrono::ceil<milliseconds>(timeout)
+    DWORD timeout_in_msec = static_cast<DWORD>(duration_cast<milliseconds>(timeout + nanoseconds{999999}).count());
+    DWORD result = ::WaitForSingleObject(hsem_, timeout_in_msec);
+    if (result != WAIT_OBJECT_0 && result != WAIT_TIMEOUT) {
+      // [thread.mutex.requirements.mutex]
+      // resource_unavailable_try_again - if any native handle type manipulated is not available.
+      throw std::system_error(std::make_error_code(std::errc::resource_unavailable_try_again), "WaitForSingleObject");
+    }
     return (result == WAIT_OBJECT_0);
   }
 
@@ -65,6 +83,7 @@ public:
   {
     // Windows.h header defines max() and min() function-like macros that make trouble,
     // we put parenthesis around `max` identifier to prevent unexpected macro expansion.
+    // https://docs.microsoft.com/windows/win32/multimedia/max
     static_assert(0 < least_max_value, "least_max_value shall be greater than zero");
     return least_max_value;
   }
@@ -73,13 +92,14 @@ public:
   {
     assert(0 <= desired && desired <= (max)());
     hsem_ = ::CreateSemaphore(NULL, (LONG)desired, (LONG)((max)()), NULL);
-    assert(hsem_ != NULL);
     // counting_semaphore constructor throws nothing.
   }
 
   ~counting_semaphore()
   {
-    ::CloseHandle(hsem_);
+    if (hsem_ != NULL) {
+      ::CloseHandle(hsem_);
+    }
   }
 
   counting_semaphore(const counting_semaphore&) = delete;
@@ -87,29 +107,42 @@ public:
 
   void release(std::ptrdiff_t update = 1)
   {
-    ::ReleaseSemaphore(hsem_, (LONG)update, NULL);
+    validate_native_handle("counting_semaphore::release");
+    BOOL result = ::ReleaseSemaphore(hsem_, (LONG)update, NULL);
+    if (!result) {
+      // [thread.mutex.requirements.mutex]
+      // resource_unavailable_try_again - if any native handle type manipulated is not available.
+      throw std::system_error(std::make_error_code(std::errc::resource_unavailable_try_again), "ReleaseSemaphore");
+    }
   }
 
   void acquire()
   {
+    validate_native_handle("counting_semaphore::acquire");
     DWORD result = ::WaitForSingleObject(hsem_, INFINITE);
-    assert(result == WAIT_OBJECT_0);
+    if (result != WAIT_OBJECT_0) {
+      // [thread.mutex.requirements.mutex]
+      // resource_unavailable_try_again - if any native handle type manipulated is not available.
+      throw std::system_error(std::make_error_code(std::errc::resource_unavailable_try_again), "WaitForSingleObject");
+    }
   }
 
   bool try_acquire() noexcept
   {
-    return (::WaitForSingleObject(hsem_, 0) == WAIT_OBJECT_0);
+    return (hsem_ != NULL && ::WaitForSingleObject(hsem_, 0) == WAIT_OBJECT_0);
   }
 
   template<class Rep, class Period>
   bool try_acquire_for(const std::chrono::duration<Rep, Period>& rel_time)
   {
+    validate_native_handle("counting_semaphore::try_acquire_for");
     return do_try_acquirewait(rel_time);
   }
 
   template<class Clock, class Duration>
   bool try_acquire_until(const std::chrono::time_point<Clock, Duration>& abs_time)
   {
+    validate_native_handle("counting_semaphore::try_acquire_until");
     return do_try_acquirewait(abs_time - Clock::now());
   }
 };
