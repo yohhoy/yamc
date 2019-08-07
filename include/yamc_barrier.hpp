0b81d5f7249db8e43f24d2aceef8632a542308c0
yoh <kawasaki.liamg@gmail.com>
2019-08-07 10:20:37 +0900

    fix include guard (yamc_barrier.hpp)

--- include/yamc_barrier.hpp
+++ include/yamc_barrier.hpp
@@ -23,8 +23,8 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-#ifndef YAMC_LATCH_HPP_
-#define YAMC_LATCH_HPP_
+#ifndef YAMC_BARRIER_HPP_
+#define YAMC_BARRIER_HPP_
 
 #include <cassert>
 #include <cstddef>
